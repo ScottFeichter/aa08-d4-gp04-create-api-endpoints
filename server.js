200f8e71e9d1d2b0f900b0aecb3615590be3166d
Scott Feichter <scottfeichter@gmail.com>
2024-04-11 13:40:20 -0700

    that just happened. or did it? it didn't

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require("http");
 
 const dogs = [
   {
     dogId: 1,
     name: "Fluffy",
-    age: 2
-  }
+    age: 2,
+  },
 ];
 
 let nextDogId = 2;
@@ -25,10 +25,11 @@ const server = http.createServer((req, res) => {
     reqBody += data;
   });
 
-  req.on("end", () => { // request is finished assembly the entire request body
+  req.on("end", () => {
+    // request is finished assembly the entire request body
     // Parsing the body of the request depending on the Content-Type header
     if (reqBody) {
-      switch (req.headers['content-type']) {
+      switch (req.headers["content-type"]) {
         case "application/json":
           req.body = JSON.parse(reqBody);
           break;
@@ -52,61 +53,91 @@ const server = http.createServer((req, res) => {
     /* ======================== ROUTE HANDLERS ======================== */
 
     // GET /dogs
-    if (req.method === 'GET' && req.url === '/dogs') {
-      // Your code here 
-
+    if (req.method === "GET" && req.url === "/dogs") {
+      // Your code here
+      res.statusCode = 200;
+      res.setHeader(`Content-Type`, `application/json`);
+      res.write(JSON.stringify(dogs));
       return res.end();
     }
 
     // GET /dogs/:dogId
-    if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/'); // ['', 'dogs', '1']
+    if (req.method === "GET" && req.url.startsWith("/dogs/")) {
+      const urlParts = req.url.split("/"); // ['', 'dogs', '1']
+
       if (urlParts.length === 3) {
-        const dogId = urlParts[2];
-        // Your code here 
+        const dogId = urlParts[2]; // MUST MAKE THIS INT
+        // Your code here
+        let parsedDogId = parseInt(dogId);
+        let resDog;
+
+        for (let dog of dogs) {
+          if (dog[parsedDogId] === dogId) {
+            resDog = dog;
+            res.statusCode = 200;
+            res.setHeader(`Content-Type`, `application/json`);
+            res.write(JSON.stringify(resDog));
+            return res.end();
+          }
+        }
+        res.statusCode = 404;
+        res.setHeader(`Content-Type`, `application/json`);
+        res.write(JSON.stringify({ message: `Dog Not Found` }));
+        return res.end();
       }
-      return res.end();
     }
 
     // POST /dogs
-    if (req.method === 'POST' && req.url === '/dogs') {
+    if (req.method === "POST" && req.url === "/dogs") {
       const { name, age } = req.body;
-      // Your code here 
-      return res.end();
+      // Your code here
+      let dogId = getNewDogId();
+      const newDog = {
+        dogId: dogId,
+        name: name,
+        age: age,
+      };
+      dogs.push(newDog);
+
+      res.statusCode === 201;
+      res.setHeader(`Content-Type`, `application/json`);
+      return res.end(JSON.stringify(newDog));
     }
 
     // PUT or PATCH /dogs/:dogId
-    if ((req.method === 'PUT' || req.method === 'PATCH')  && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+    if (
+      (req.method === "PUT" || req.method === "PATCH") &&
+      req.url.startsWith("/dogs/")
+    ) {
+      const urlParts = req.url.split("/");
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here 
+        // Your code here
       }
+
       return res.end();
     }
 
     // DELETE /dogs/:dogId
-    if (req.method === 'DELETE' && req.url.startsWith('/dogs/')) {
-      const urlParts = req.url.split('/');
+    if (req.method === "DELETE" && req.url.startsWith("/dogs/")) {
+      const urlParts = req.url.split("/");
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
-        // Your code here 
+        // Your code here
       }
       return res.end();
     }
 
     // No matching endpoint
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'application/json');
-    return res.end('Endpoint not found');
+    res.setHeader("Content-Type", "application/json");
+    return res.end("Endpoint not found");
   });
-
 });
 
-
 if (require.main === module) {
-    const port = 8000;
-    server.listen(port, () => console.log('Server is listening on port', port));
+  const port = 8000;
+  server.listen(port, () => console.log("Server is listening on port", port));
 } else {
-    module.exports = server;
+  module.exports = server;
 }
